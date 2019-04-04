a1c10e9bef38d9be7814fc1ba284746df29907a4
Jasper King-Harman <j.kingharman@googlemail.com>
2019-04-04 10:54:26 +0100

    Remove write endpoints from SpotTypeAPI

--- lib/grey/apis/spot_type_api.rb
+++ lib/grey/apis/spot_type_api.rb
@@ -6,7 +6,7 @@ module Grey
     version 'v0', using: :path
 
     format :json
-    default_error_status :json
+      default_error_status :json
 
     helpers do
       include Api::Helpers
@@ -28,32 +28,6 @@ module Grey
       get ":id" do
         spot_type = Models::SpotType.find_by(id: params[:id]) || raise(ApiError::NotFound)
         serialize(spot_type)
-      end
-
-      post do
-        authorize!
-        required_params!(:spot_type)
-        spot_type = Models::SpotType.create!(
-          name: params[:spot_type][:name], slug: params[:spot_type][:slug],
-        )
-        serialize(spot_type)
-      end
-
-      put ":id" do
-        authorize!
-        required_params!(:spot_type)
-        spot_type = Models::SpotType.find_by(id: params[:id]) || raise(ApiError::NotFound)
-        spot_type.update(
-          name: params[:spot_type][:name], slug: params[:spot_type][:slug]
-        )
-        serialize(spot_type)
-      end
-
-      delete ":id" do
-        authorize!
-        spot_type = Models::SpotType.find_by(id: params[:id]) || raise(ApiError::NotFound)
-        spot_type.destroy
-        {}.to_json
       end
     end
   end
