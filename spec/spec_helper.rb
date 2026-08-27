efe3a5e8d7c2070acbca4a5bca36bba7c0235978
John King-Harman <j.kingharman@googlemail.com>
2026-08-27 16:35:16 +0100

    Run specs in random order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -139,4 +139,7 @@ RSpec.configure do |config|
   #   # test failures related to randomization by passing the same `--seed` value
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
+
+  config.order = :random
+  Kernel.srand config.seed
 end
